61b3140c2bcea5f68ebcb05671522e03636c1ec8
rodries <rodries@gmail.com>
2012-06-02 12:27:53 +0200

    fix ro.aokp.version
    Change-Id: Iba1512c91457d307cd0db016f7b46ddbd7efc206

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -4,8 +4,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76I BUILD_VERSION_TAGS=relea
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.rommanager.developerid=teamkang \
-    ro.aokp.version=$(TARGET_PRODUCT)_build-37
-    ro.aokp.version+=_rodries_$(shell date +"%Y%m%d")
+    ro.aokp.version=$(TARGET_PRODUCT)_build-37_rodries_$(shell date +"%Y%m%d")
     
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
