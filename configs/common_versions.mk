0fe3e8d0821bd782fd929bb70e8137e5a706bef0
rodries <rodries@gmail.com>
2012-06-02 11:33:18 +0200

    added date to ro.aokp.version
    Change-Id: I4a69ec3a2ed74f1fc15e106841778007def13e87

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -5,7 +5,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76I BUILD_VERSION_TAGS=relea
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.rommanager.developerid=teamkang \
     ro.aokp.version=$(TARGET_PRODUCT)_build-37
-
+    ro.aokp.version+=_rodries_$(shell date +"%Y%m%d")
+    
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=aokp \
